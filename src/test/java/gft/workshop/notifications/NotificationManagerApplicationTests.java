d0364f4e8681c3420608fb454944c5cd33312754
Amorós López <mraz@gft.com>
2024-10-10 15:41:17 +0200

    initial commit

---
+++ src/test/java/gft/workshop/notifications/NotificationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gft.workshop.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
